541c8ffc7fca8028b950ff3aea154a1acff42197
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2025-01-09 18:25:14 +0700

    refactor: response for ticket usage

--- controller/ticket_usage.go
+++ controller/ticket_usage.go
@@ -10,6 +10,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var responseTicketUsageName = "ticket usage"
+var responseTicketUsage helper.ResponseMessage
+
 type TicketUsageHanlder struct {
 	repository entity.TicketUsageRepository
 }
@@ -23,77 +26,77 @@ func NewTicketUsageHandler(repo entity.TicketUsageRepository) *TicketUsageHanlde
 func (h *TicketUsageHanlder) GetMany(ctx *gin.Context) {
 	ticketUsages, err := h.repository.GetMany(context.Background())
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed to fetch ticket usages"))
+		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse(responseTicketUsage.GetFailed(responseTicketUsageName)))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, helper.SuccessResponse("Fetch data ticket usages successfully", ticketUsages))
+	ctx.JSON(http.StatusOK, helper.SuccessResponse(responseTicketUsage.GetSuccessfully(responseTicketUsageName), ticketUsages))
 }
 
 func (h *TicketUsageHanlder) GetOne(ctx *gin.Context) {
 	ticketUsageId, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed to fetch ticket usage"))
+		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse(responseTicketUsage.GetFailed(responseTicketUsageName)))
 		return
 	}
 
 	ticketUsage, err := h.repository.GetOne(context.Background(), uint(ticketUsageId))
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed to fetch ticket usage"))
+		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse(responseTicketUsage.GetFailed(responseTicketUsageName)))
 		return
 	}
-	ctx.JSON(http.StatusOK, helper.SuccessResponse("Fetch data ticket usage succesfully", ticketUsage))
+	ctx.JSON(http.StatusOK, helper.SuccessResponse(responseTicketUsage.GetSuccessfully(responseTicketUsageName), ticketUsage))
 }
 
 func (h *TicketUsageHanlder) CreateOne(ctx *gin.Context) {
 	var ticketUsage entity.TicketUsage
 	if err := ctx.ShouldBindJSON(&ticketUsage); err != nil {
-		ctx.JSON(http.StatusBadRequest, helper.FailedResponse("Invalid request payload"))
+		ctx.JSON(http.StatusBadRequest, helper.FailedResponse(responseTicketUsage.RequestFailed(responseTicketUsageName)))
 		return
 	}
 
 	createTicketUsage, err := h.repository.CreateOne(context.Background(), &ticketUsage)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed to create ticket usage"))
+		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse(responseTicketUsage.GetFailed(responseTicketUsageName)))
 		return
 	}
-	ctx.JSON(http.StatusOK, helper.SuccessResponse("Craate ticket usage successfully", createTicketUsage))
+	ctx.JSON(http.StatusOK, helper.SuccessResponse(responseTicketUsage.CreateSuccessfully(responseTicketUsageName), createTicketUsage))
 }
 
 func (h *TicketUsageHanlder) UpdateOne(ctx *gin.Context) {
 	ticketUsageId, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, helper.FailedResponse("Invalid ticket usage ID"))
+		ctx.JSON(http.StatusBadRequest, helper.FailedResponse(responseTicketUsage.IdFailed(responseTicketUsageName)))
 		return
 	}
 
 	var updateData map[string]interface{}
 	if err := ctx.ShouldBindJSON(&updateData); err != nil {
-		ctx.JSON(http.StatusBadRequest, helper.FailedResponse("Invalid request payload"))
+		ctx.JSON(http.StatusBadRequest, helper.FailedResponse(responseTicketUsage.GetFailed(responseTicketUsageName)))
 		return
 	}
 
 	updateRefund, err := h.repository.UpdateOne(context.Background(), uint(ticketUsageId), updateData)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed to update ticket usage"))
+		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse(responseTicketUsage.UpdateFailed(responseTicketUsageName)))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, helper.SuccessResponse("Update ticket usage successfully", updateRefund))
+	ctx.JSON(http.StatusOK, helper.SuccessResponse(responseTicketUsage.UpdateSuccessfully(responseTicketUsageName), updateRefund))
 
 }
 
 func (h *TicketUsageHanlder) DeleteOne(ctx *gin.Context) {
 	ticketUsageId, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, helper.FailedResponse("Invalid ticket usage ID"))
+		ctx.JSON(http.StatusBadRequest, helper.FailedResponse(responseTicketUsage.IdFailed(responseTicketUsageName)))
 		return
 	}
 
 	if err := h.repository.DeleteOne(context.Background(), uint(ticketUsageId)); err != nil {
-		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed to delete ticket usage"))
+		ctx.JSON(http.StatusInternalServerError, helper.FailedResponse(responseTicketUsage.DeleteFailed(responseTicketUsageName)))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, helper.SuccessResponse("Delete ticket usage successfully", nil))
+	ctx.JSON(http.StatusOK, helper.SuccessResponse(responseTicketUsage.DeleteSuccessfully(responseTicketUsageName), nil))
 }
